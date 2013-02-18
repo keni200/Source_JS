53a1e91c95be7036fd7c203e03eacb23b5a393e0
keni200 <jojoman2@naver.com>
2013-02-18 14:53:14 +0900

    ..

--- Template/contents/MongoDB/insert.js
+++ Template/contents/MongoDB/insert.js
@@ -1,6 +1,6 @@
 /*
 	req : Get
-	http://127.0.0.1:20080/log/f?destination=ponggameLog&body={%22I_LogId%22:3,%22I_LogDetailId%22:1,%22I_ChannelUserId%22:%22111%22,%22I_PCSeq%22:%221%22,%22I_LogDes%22:{%22%20IviteChannelUserID%22:%20%22$CHANNEL_USER_ID%22},%20%22I_ConnectIP%22:%220%22}
+	http://127.0.0.1:20080/MongoDB?{jsonKey:jsonValue}
 */
 
 var mongodb = require('mongodb');
@@ -18,11 +18,6 @@ exports = module.exports = function (req, res, next) {
 	var currentTime = new Date()	
 	var date = util.getTimeStamp(currentTime);
 
-	var shardKey = Math.floor(Math.random() * 1000) + currentTime.getTime();
-	bodyData._shard = shardKey.toString();
-	bodyData.I_LogDatetime = (currentTime.getTime().toString());
-	bodyData.date = date.substr(0,10);
-	bodyData.dateTime = date.substr(11, date.length);
 	//console.log(bodyData);
 
 	var db = mongoDBPool.getPool();
